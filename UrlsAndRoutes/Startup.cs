afef73bd3afa9200c1717803f8402b6fd909570e
Jon Perkins <captainperk81@gmail.com>
2018-07-12 21:37:59 -0400

    Creating and Registering a Simple Route

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -16,7 +16,7 @@ namespace UrlsAndRoutes
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvc();
+            app.UseMvc(routes => routes.MapRoute(name: "default", template: "{controller}/{action}"));
         }
     }
 }
