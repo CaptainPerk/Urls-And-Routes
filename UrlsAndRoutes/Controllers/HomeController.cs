4878c4e9214de59be614293758fc28c1a4258095
Jon Perkins <captainperk81@gmail.com>
2018-07-15 19:25:50 -0400

    Defining Custom Segment Variables

--- UrlsAndRoutes/Controllers/HomeController.cs
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -6,5 +6,12 @@ namespace UrlsAndRoutes.Controllers
     public class HomeController : Controller
     {
         public ViewResult Index() => View("Result", new Result {Controller = nameof(HomeController), Action = nameof(Index)});
+
+        public ViewResult CustomVariable()
+        {
+            Result result = new Result{ Controller = nameof(HomeController), Action = nameof(CustomVariable)};
+            result.Data["id"] = RouteData.Values["id"];
+            return View("Result", result);
+        }
     }
 }
