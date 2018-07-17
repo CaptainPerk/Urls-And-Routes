658ee588b3d1ede03bdb64e5b2b65c7ddd46457e
Jon Perkins <captainperk81@gmail.com>
2018-07-17 06:46:21 -0400

    Applying Attribute Routing

--- UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -5,6 +5,7 @@ namespace UrlsAndRoutes.Controllers
 {
     public class CustomerController : Controller
     {
+        [Route("myroute")]
         public ViewResult Index() => View("Result", new Result { Controller = nameof(CustomerController), Action = nameof(Index)});
 
         public ViewResult List(string id)
