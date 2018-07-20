3eec3345878e7a05fbd73dd75b3b4bfd9fd27ff9
Jon Perkins <captainperk81@gmail.com>
2018-07-20 06:20:47 -0400

    Generating a URL from a Specific Route

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -23,6 +23,7 @@ namespace UrlsAndRoutes
             {
                 //routes.MapRoute(name: "NewRoute", template: "App/Do{action}", defaults: new {controller = "Home"});
                 routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "out", template: "outbound/{controller=Home}/{action=Index}");
             });
         }
     }
