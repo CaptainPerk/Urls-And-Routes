c10abdca3442f494e30394502646429a5af0697e
Jon Perkins <captainperk81@gmail.com>
2018-07-21 12:46:04 -0400

    Creating an Area Route

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -26,6 +26,7 @@ namespace UrlsAndRoutes
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
+                routes.MapRoute(name: "areas", template: "{area:exists}/{controller=Home}/{action=Index}");
                 routes.Routes.Add(new LegacyRoute(app.ApplicationServices, "/articles/Windows_3.1_Overview.html", "/old/.NET_1.0_Class_Library"));
                 routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
                 routes.MapRoute(name: "out", template: "outbound/{controller=Home}/{action=Index}");
